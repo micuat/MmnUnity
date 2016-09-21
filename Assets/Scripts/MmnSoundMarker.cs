7f6b33194655625b505565957b6ea292698365e6
naoto <micuat@gmail.com>
2016-09-21 14:08:15 -0400

    Auditory MMN

---
+++ Assets/Scripts/MmnSoundMarker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MmnSoundMarker : MmnMarker {
+
+	public AudioSource wavStandard;
+	public AudioSource wavDeviant;
+
+	// Use this for initialization
+	override protected void Start () {
+		base.Start ();
+	}
+
+	// Update is called once per frame
+	override protected void Update () {
+		base.Update ();
+	}
+
+	override protected void presentStandard()
+	{
+		wavStandard.Play ();
+	}
+
+	override protected void presentDeviant()
+	{
+		wavDeviant.Play ();
+	}
+}
