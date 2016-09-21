a1f7e56a3091dbc97c899b55391b87957cb9282b
naoto <micuat@gmail.com>
2016-09-21 13:34:06 -0400

    Better random sequence generator

--- Assets/Scripts/MmnMarker.cs
+++ Assets/Scripts/MmnMarker.cs
@@ -9,39 +9,71 @@ public class MmnMarker : MonoBehaviour {
 
 	public GameObject[] stimulusObjects;
 	public LSLMarkerStream markerStream;
-	public float visibleDuration = 1.0f;
-	public float invisibleDuration = 1.0f;
+	public float timeDuration = 0.5f;
+	private float timeISI;
+	public float timeSOA = 1.0f;
+	public int numStimuli;
+	public float pDeviant;
 
 	void Start () {
 		Assert.IsNotNull(markerStream, "You forgot to assign the reference to a marker stream implementation!");
-
-		if (markerStream != null)
-			StartCoroutine(WriteContinouslyMarkerEachSecond());
 
 		Assert.IsFalse (stimulusObjects.Length < 2, "Not enough stimulus objects are defined");
 
 		foreach (var obj in stimulusObjects) {
 			obj.GetComponent<Renderer> ().enabled = false;
 		}
+
+		timeISI = timeSOA - timeDuration;
+		Assert.IsTrue (timeISI > 0, "SOA must be longer than stimuls duration");
+		Assert.IsTrue (pDeviant > 0 && pDeviant < 0.5, "deviant probability must be within 0 and 0.5");
 	}
-	
+
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space) && markerStream != null)
+			StartCoroutine(WriteContinouslyMarkerEachSecond());
+	}
+
 	IEnumerator WriteContinouslyMarkerEachSecond()
 	{
-		while (true)
+		int meanStandardsInRow = (int)((1.0f - pDeviant) / pDeviant);
+		int minStandardsInRow = meanStandardsInRow / 2;
+		int maxStandardsInRow = meanStandardsInRow * 3 / 2;
+		int nextDeviant = 1 + UnityEngine.Random.Range (minStandardsInRow, maxStandardsInRow);
+
+		for(int i = 0; i < numStimuli; i++)
 		{
-			var currentIndex = GetARandomMarker();
-			markerStream.Write(stimulusObjects[currentIndex].name);
-			stimulusObjects[currentIndex].GetComponent<Renderer> ().enabled = true;
-			yield return new WaitForSecondsRealtime(visibleDuration);
-			foreach (var obj in stimulusObjects) {
-				obj.GetComponent<Renderer> ().enabled = false;
+			if (i == nextDeviant) {
+				markerStream.Write("Deviant");
+				presentDeviant ();
+
+				nextDeviant = i + 1 + UnityEngine.Random.Range (minStandardsInRow, maxStandardsInRow);
 			}
-			yield return new WaitForSecondsRealtime(invisibleDuration);
+			else {
+				markerStream.Write ("Standard");
+				presentStandard ();
+			}
+			yield return new WaitForSecondsRealtime(timeDuration);
+
+			presentBreak();
+			yield return new WaitForSecondsRealtime(timeISI);
 		}
 	}
 
-	private int GetARandomMarker()
+	void presentStandard()
 	{
-		return UnityEngine.Random.value > 0.8 ? 0 : 1;
+		stimulusObjects[0].GetComponent<Renderer> ().enabled = true;
+	}
+
+	void presentDeviant()
+	{
+		stimulusObjects[1].GetComponent<Renderer> ().enabled = true;
+	}
+
+	void presentBreak()
+	{
+		foreach (var obj in stimulusObjects) {
+			obj.GetComponent<Renderer> ().enabled = false;
+		}
 	}
 }
