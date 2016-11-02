7989697899c76f6b5d0d6b2c6beac8dfe87b88bf
micuat <micuat@gmail.com>
2016-11-02 12:07:18 -0400

    Add settings to on/off 3d environment; make sceneController a singleton

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,11 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SceneController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		UnityEngine.VR.VRSettings.enabled = false;
+    private bool _Is3DEnv;
+    public bool Is3DEnv
+    {
+        get { return instanceRef._Is3DEnv; }
+        set { instanceRef._Is3DEnv = value; }
+    }
+
+    private static SceneController instanceRef;
+    public static SceneController InstanceRef
+    {
+        get { return instanceRef; }
+    }
+
+    public Toggle guiEnv;
+
+    void Awake()
+    {
+        if (instanceRef == null)
+        {
+            instanceRef = this;
+            DontDestroyOnLoad(gameObject);
+            Is3DEnv = false;
+        }
+        else
+        {
+            // don't kill me... GUI requires this object
+            //DestroyImmediate(gameObject);
+        }
+
+        UnityEngine.VR.VRSettings.enabled = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        guiEnv.isOn = Is3DEnv;
 	}
 
 	// Update is called once per frame
--- Assets/Scripts/StageController.cs
+++ Assets/Scripts/StageController.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class StageController : MonoBehaviour {
     public bool setVr = false;
     private TouchScript.Gestures.FlickGesture gesture;
+    private SceneController sceneController;
+    public GameObject environmentObject;
 
     // Use this for initialization
     void Start () {
@@ -11,6 +13,24 @@ public class StageController : MonoBehaviour {
 
         gesture = GetComponent<TouchScript.Gestures.FlickGesture>();
         gesture.Flicked += flickedHandler;
+
+        sceneController = SceneController.InstanceRef;
+        if(sceneController.Is3DEnv)
+        {
+            if (environmentObject != null)
+            {
+                environmentObject.SetActive(true);
+            }
+        }
+        else
+        {
+            if (environmentObject != null)
+            {
+                environmentObject.SetActive(false);
+            }
+            Camera.main.clearFlags = CameraClearFlags.Color;
+            Camera.main.backgroundColor = Color.black;
+        }
     }
 
     // Update is called once per frame
